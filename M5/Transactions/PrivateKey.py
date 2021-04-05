b9a5aa24c02c6409105333e15d32cd539e2171a3
Linus Eriksson <linus.ca.eriksson@gmail.com>
2021-04-05 16:37:40 +0200

    Add key memory

--- M5/Transactions/PrivateKey.py
+++ M5/Transactions/PrivateKey.py
@@ -44,14 +44,20 @@ class ElipsisPoint:
 
 
 class CoinKey:
-    private_key: int
-    name: str
-    public_key_point: ElipsisPoint
-    uncompressed: int
-    compressed: int
 
     def __init__(self):
-        pass
+        self.private_key = None
+        self.public_key_point = None
+        self.uncompressed = None
+        self.compressed = None
+        self.uncom_hash160 = None
+        self.com_hash160 = None
+        self.uncom_bitcoin = None
+        self.com_bitcoin = None
+        self.uncom_wif = None
+        self.com_wif = None
+        self.segwit_p2sh = None
+        self.segwit_p2wpkh = None
 
     # Double sha256 function, taken from https://colab.research.google.com/drive/1Eb4bNE8HU9sULhEqEXCWwuQ9UkG-xsm4
     @classmethod
@@ -84,77 +90,97 @@ class CoinKey:
         random.seed(seed)
         self.private_key = random.randrange(0, _r)
 
-    def generate_public_key(self, generator: secpGen = GENERATOR):
-        if self.private_key is None:
-            raise ValueError("Can not generate public key without private key!")
-        (x, y) = self.private_key * generator
-        self.public_key_point = ElipsisPoint(x, y)
+    def generate_public_key(self, generator: secpGen = GENERATOR, regenerate=False):
+        if self.public_key_point is None or regenerate:
+            if self.private_key is None:
+                raise ValueError("Can not generate public key without private key!")
+            (x, y) = self.private_key * generator
+            self.public_key_point = ElipsisPoint(x, y)
 
     def get_uncompressed(self):
-        if self.public_key_point is None:
-            raise ValueError("Public key not generated!")
-        return self.public_key_point.get_uncompressed()
+        if self.uncompressed is None:
+            if self.public_key_point is None:
+                raise ValueError("Public key not generated!")
+            self.uncompressed = self.public_key_point.get_uncompressed()
+        return self.uncompressed
 
     def get_compressed(self):
-        if self.public_key_point is None:
-            raise ValueError("Public key not generated!")
-        return self.public_key_point.get_compressed()
+        if self.compressed is None:
+            if self.public_key_point is None:
+                raise ValueError("Public key not generated!")
+            self.compressed = self.public_key_point.get_compressed()
+        return self.compressed
 
     def get_uncompressed_hash160_address(self):
-        hashed = CoinKey._hash160(self.get_uncompressed())
-        return hashed
+        if self.uncom_hash160 is None:
+            self.uncom_hash160 = CoinKey._hash160(self.get_uncompressed())
+        return self.uncom_hash160
 
     def get_compressed_hash160_address(self):
-        hashed = CoinKey._hash160(self.get_compressed())
-        return hashed
+        if self.com_hash160 is None:
+            self.com_hash160 = CoinKey._hash160(self.get_compressed())
+        return self.com_hash160
 
     def get_uncompressed_bitcoin_address(self, prefix: str):
-        data = prefix + self.get_uncompressed_hash160_address()
-        address = CoinKey._create_base58_with_checksum(data)
-        return address
+        if self.uncom_bitcoin is None:
+            data = prefix + self.get_uncompressed_hash160_address()
+            self.uncom_bitcoin = CoinKey._create_base58_with_checksum(data)
+        return self.uncom_bitcoin
 
     def get_compressed_bitcoin_address(self, prefix: str):
-        data = prefix + self.get_compressed_hash160_address()
-        address = CoinKey._create_base58_with_checksum(data)
-        return address
+        if self.com_bitcoin is None:
+            data = prefix + self.get_compressed_hash160_address()
+            self.com_bitcoin = CoinKey._create_base58_with_checksum(data)
+        return self.com_bitcoin
 
-    def get_wif_format(self, compressed=True):
-        hex_key = "%064x" % self.private_key
-        if compressed:
+    def get_compressed_wif_format(self):
+        if self.private_key is None:
+            raise ValueError("Can not get WIF format without private key!")
+        if self.com_wif is None:
+            hex_key = "%064x" % self.private_key
             suffix = "01"
-        else:
-            suffix = ""
-        data = "80" + hex_key + suffix
-        wif = CoinKey._create_base58_with_checksum(data)
-        return wif
+            data = "80" + hex_key + suffix
+            self.com_wif = CoinKey._create_base58_with_checksum(data)
+        return self.com_wif
+
+    def get_uncompressed_wif_format(self):
+        if self.private_key is None:
+            raise ValueError("Can not get WIF format without private key!")
+        if self.uncom_wif is None:
+            hex_key = "%064x" % self.private_key
+            data = "80" + hex_key
+            self.uncom_wif = CoinKey._create_base58_with_checksum(data)
+        return self.uncom_wif
 
     def get_p2sh_segwit(self):
-        compressed = self.get_compressed_hash160_address()
-        with_signature = "0014" + compressed
-        hashed_signature = CoinKey._hash160(with_signature)
-        data = "05" + hashed_signature
-        segwit_address = CoinKey._create_base58_with_checksum(data)
-        return segwit_address
+        if self.segwit_p2sh is None:
+            compressed = self.get_compressed_hash160_address()
+            with_signature = "0014" + compressed
+            hashed_signature = CoinKey._hash160(with_signature)
+            data = "05" + hashed_signature
+            self.segwit_p2sh = CoinKey._create_base58_with_checksum(data)
+        return self.segwit_p2sh
 
     def get_p2wpkh_segwit(self, witness_version: int):
-        compressed = self.get_compressed_hash160_address()
-        prefix = "bc"
-        segwit_address = segwit_addr.encode(prefix, witness_version, binascii.unhexlify(compressed))
-        return segwit_address
+        if self.segwit_p2wpkh is None:
+            compressed = self.get_compressed_hash160_address()
+            prefix = "bc"
+            self.segwit_p2wpkh = segwit_addr.encode(prefix, witness_version, binascii.unhexlify(compressed))
+        return self.segwit_p2wpkh
 
     def __str__(self):
         if self.public_key_point is None:
             raise ValueError("No public key generated!")
         retstr = f"Private key: {self.private_key}\n" \
-                 f"Elipsis: x: {self.public_key_point.x} y: {self.public_key_point.y}\n" \
+                 f"Public key point: x: {self.public_key_point.x} y: {self.public_key_point.y}\n" \
                  f"Uncompressed public key: {self.get_uncompressed()}\n" \
                  f"Compressed public key: {self.get_compressed()}\n" \
-                 f"Uncompressed Hash160 address: {self.get_uncompressed_hash160_address()}\"" \
+                 f"Uncompressed Hash160 address: {self.get_uncompressed_hash160_address()}\n" \
                  f"Compressed Hash160 address: {self.get_compressed_hash160_address()}\n" \
                  f"Uncompressed Bitcoin address: {self.get_uncompressed_bitcoin_address('00')}\n" \
                  f"Compressed Bitcoin address: {self.get_compressed_bitcoin_address('00')}\n" \
-                 f"Uncompressed wif: {self.get_wif_format(False)}\n" \
-                 f"Compressed wif: {self.get_wif_format()}\n" \
+                 f"Uncompressed wif: {self.get_uncompressed_wif_format()}\n" \
+                 f"Compressed wif: {self.get_compressed_wif_format()}\n" \
                  f"Segwit P2SH: {self.get_p2sh_segwit()}\n" \
                  f"Segwit P2WPKH: {self.get_p2wpkh_segwit(0)}"
         return retstr
